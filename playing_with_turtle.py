97e989d85b060760f4c43762f58cf3d9ce410ab5
rohan209547mourya <rohan.209547@gmail.com>
2023-03-13 22:49:54 +0530

    fun with turtle

---
+++ playing_with_turtle.py
@@ -0,0 +1,20 @@
+import turtle
+from turtle import *
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+
+# t.right(180)
+# t.forward(90)
+# t.left(90)
+# t.back(90)
+
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+
+t.circle(100)
---
+++ random_word_module.py
@@ -0,0 +1,5 @@
+from random_word import RandomWords
+
+word_generator = RandomWords()
+
+print(word_generator.get_random_word())
